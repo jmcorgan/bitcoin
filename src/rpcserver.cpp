c8d4fb447afb451cc804d7910d99844a950b0b02
dexX7 <dexx@bitwatch.co>
2014-05-03 20:23:48 +0200

    Added needed adjustments for "listalltransactions"

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -257,6 +257,7 @@ static const CRPCCommand vRPCCommands[] =
     { "decoderawtransaction",   &decoderawtransaction,   false,     false,      false },
     { "decodescript",           &decodescript,           false,     false,      false },
     { "getrawtransaction",      &getrawtransaction,      false,     false,      false },
+    { "listalltransactions",    &listalltransactions,    false,     false,      false },
     { "sendrawtransaction",     &sendrawtransaction,     false,     false,      false },
     { "signrawtransaction",     &signrawtransaction,     false,     false,      false }, /* uses wallet if enabled */
 
